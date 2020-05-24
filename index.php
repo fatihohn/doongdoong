1e8fbbfa7e6f8be6782d94f435ca4a36bb64c4d4
fatihohn <fatihohn@gmail.com>
2020-05-25 00:54:26 +0900

    modify index

--- index.php
+++ index.php
@@ -7,22 +7,7 @@
 <body>
     <div id="bbdd_body">
         <header id="bbdd_hd">
-            <!-- <div id="bbdd_hd_wrap">
-                <div id="bbdd_hd_area" style="transform: translate3d(0px, 0px, 0px); position: fixed; top: 0px;">
-                    <div class="hd_contain">
-                        <div class="hd_logo">
-                            <a href="./">
-                                <img src="static/img/logo.png" alt="변방둥둥">
-                            </a>
-                        </div>
-                        <div class="hd_menu">
-                            <a>
-                                <img src="static/img/menu-bar.png" alt="메뉴바">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
+        
             <?php include "front_header.php"; ?>
         </header>
 
@@ -33,12 +18,9 @@
             <div class="sc_contain">
                 <div class="sc_list_area">
                     <ul class="sc_list_contain">
-                    <!-- <ul class="sc_list_con"> -->
                         <?php include "frontList.php"; ?>
                            
                 
-                <!-- </ul>
-                </div> -->
                 
             </div>
         </div>
@@ -47,15 +29,7 @@
 
 
         <footer id="bbdd_ft">
-            <!-- <div id="bbdd_ft_wrap">
-                <div id="bbdd_ft_area">
-                    <div class="ft_contain">
-                        <p class="gg-batang">
-                            COPYRIGHT 2020 변방의북소리.
-                        </p>
-                    </div>
-                </div>
-            </div> -->
+         
 
             <?php include "footer.php";?>
         </footer>
