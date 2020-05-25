cdc25983eb73cb538eb988ace4026881994d40f6
fatihohn <fatihohn@gmail.com>
2020-05-25 19:15:06 +0900

    create thumbs

--- admin_create_thumbs_action.php
+++ admin_create_thumbs_action.php
@@ -64,7 +64,8 @@ $result = mysqli_query($conn, $sqlThumbs);
     if($result === false){
         // if($result === false || $resultUpdate === false){
             echo '저장실패. 관리자에게 문의해주세요';
-            echo error_log(mysqli_error($conn));
+            echo strval(error_log(mysqli_error($conn)));
+            error_log(mysqli_error($conn));
         }
         else{
             echo("<script>alert('연재물이 생성되었습니다.');location.href='admin_thumbsList.php';</script>");
