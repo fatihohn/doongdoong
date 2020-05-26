4cb6f54ff74d8db3db32a309742ade689d3429f0
fatihohn <fatihohn@gmail.com>
2020-05-26 14:13:55 +0900

    create thumbs15

--- admin_create_thumbs_action.php
+++ admin_create_thumbs_action.php
@@ -63,9 +63,10 @@ $result = mysqli_query($conn, $sql);
 
     if($result === false){
         // if($result === false || $resultUpdate === false){
-            echo '저장실패. 관리자에게 문의해주세요<br>'.$sql.'<br>'.$result.'<br>'. mysqli_connect_error($conn);
+            echo '저장실패. 관리자에게 문의해주세요<br>'.$sql.'<br>'.$result.'<br>'.mysqli_connect_error($conn);
             // echo '저장실패. 관리자에게 문의해주세요<br>'.$sql.'<br>'.$img.'<br>'.$img_dir.'<br>'.$result.'<br>'. mysqli_connect_error($conn);
             error_log(mysqli_error($conn));
+            error_log(mysqli_connect_error($conn));
         }
         else{
             echo("<script>alert('연재물이 생성되었습니다.');location.href='admin_thumbsList.php';</script>");
