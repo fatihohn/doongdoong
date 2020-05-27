c0a692b58b27e89fc9420ab2e09022a8f13e7502
fatihohn <fatihohn@gmail.com>
2020-05-27 21:11:32 +0900

    admin footer

--- admin_footer.php
+++ admin_footer.php
@@ -1,9 +1,8 @@
 <?php
  echo'
  <div id="footercontent">
-            <p>변방의북소리 대표자 : 이경렬</p>
-            <p>변방의북소리 개발자 : 온동훈</p>
-            <p>Copyright 2020 © 변방의북소리. All rights reserved</p>
+ <p>Copyright 2020 © 변방의북소리. All rights reserved</p>
+            <p>변방의북소리 대표자 : 이경렬 | 개발자 : 온동훈</p>
         </div>
   ';
 
