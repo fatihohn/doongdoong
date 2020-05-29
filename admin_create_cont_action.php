f40943ff252f47f4315838b122a38ff559478a82
fatihohn <fatihohn@gmail.com>
2020-05-29 16:24:51 +0900

    create cont pre

--- admin_create_cont_action.php
+++ admin_create_cont_action.php
@@ -63,68 +63,68 @@ $memo = mysqli_real_escape_string($conn, $memo);
                 }
                 
                 
-                // $sql = "
-                // INSERT INTO contents
-                //         (no, author, username, category, sess, zin, title, content, display, memo, created)
-                //     VALUES(
-                    //         '{$no}',
-                    //         '{$author}',
-                    //         '{$username}',
-                    //         '{$category}',
-                    //         '{$sess}',
-                    //         '{$zin}',
-                    //         '{$title}',
-                    //         '{$content}',
-                    //         '{$display}',
-                    //         '{$memo}',
-                    //         NOW()
-                    //         )";
+                $sql = "
+                INSERT INTO contents
+                        (no, author, username, category, sess, zin, title, content, display, memo, created)
+                    VALUES(
+                            '{$no}',
+                            '{$author}',
+                            '{$username}',
+                            '{$category}',
+                            '{$sess}',
+                            '{$zin}',
+                            '{$title}',
+                            '{$content}',
+                            '{$display}',
+                            '{$memo}',
+                            NOW()
+                            )";
+                            
+                            $result = mysqli_query($conn, $sql);
+                            if($result === false){
+                                echo '저장실패. 관리자에게 문의해주세요';
+                                error_log(mysqli_error($conn));
+                            }
+                            else{
+                                echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
+                            }
                     
                     
-                    
-                    $created = mysqli_real_escape_string($conn, NOW());
-                    $sql = "
-                    INSERT INTO contents
-                            (no, author, username, category, sess, zin, title, content, display, memo, created)
-                        VALUES(
-                                ?,
-                                ?,
-                                ?,
-                                ?,
-                                ?,
-                                ?,
-                                ?,
-                                ?,
-                                ?,
-                                ?,
-                                ?
-                                )";
+        //             $created = mysqli_real_escape_string($conn, NOW());
+        //             $sql = "
+        //             INSERT INTO contents
+        //                     (no, author, username, category, sess, zin, title, content, display, memo, created)
+        //                 VALUES(
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?,
+        //                         ?
+        //                         )";
 
-        $stmt = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($stmt, $sql)) {
-                echo "sql error";
-        } else {
-                mysqli_stmt_bind_param($stmt, "issssssssss", $no, $author, $username, $category, $sess, $zin, $title, $content, $display, $memo, $created);
-                mysqli_stmt_execute($stmt);
-                $result = mysqli_stmt_get_result($stmt);
-                if($result === false){
-                    echo '저장실패. 관리자에게 문의해주세요';
-                    error_log(mysqli_error($conn));
-                }
-                else{
-                    echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
-                }
-                // mysqli_stmt_close();
-            }
+        // $stmt = mysqli_stmt_init($conn);
+        // if (!mysqli_stmt_prepare($stmt, $sql)) {
+        //         echo "sql error";
+        // } else {
+        //         mysqli_stmt_bind_param($stmt, "issssssssss", $no, $author, $username, $category, $sess, $zin, $title, $content, $display, $memo, $created);
+        //         mysqli_stmt_execute($stmt);
+        //         $result = mysqli_stmt_get_result($stmt);
+        //         if($result === false){
+        //             echo '저장실패. 관리자에게 문의해주세요';
+        //             error_log(mysqli_error($conn));
+        //         }
+        //         else{
+        //             echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
+        //         }
+        //         // mysqli_stmt_close();
+        //     }
             
-            // $result = mysqli_query($conn, $sql);
-            // if($result === false){
-            //     echo '저장실패. 관리자에게 문의해주세요';
-            //     error_log(mysqli_error($conn));
-            // }
-            // else{
-            //     echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
-            // }
             
 
 
