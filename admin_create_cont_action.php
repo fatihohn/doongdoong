aca2ac7989fee2fb5c5b500756d16383260c232a
fatihohn <fatihohn@gmail.com>
2020-05-29 17:53:52 +0900

    create cont prepare

--- admin_create_cont_action.php
+++ admin_create_cont_action.php
@@ -33,97 +33,97 @@ $memo = $_POST['memo'];
 $memo = mysqli_real_escape_string($conn, $memo);
 
 
-// $id = "id";
-// $sqlNo = "SELECT `no` FROM contents ORDER BY ? DESC LIMIT 1";
-// $stmt = mysqli_stmt_init($conn);
-// if (!mysqli_stmt_prepare($stmt, $sqlNo)) {
-    //     echo "sqlNo error";
-    // } else {
-        //     mysqli_stmt_bind_param($stmt, "s", $id);
-        //     mysqli_stmt_execute($stmt);
-        //     $resultNo = mysqli_stmt_get_result($stmt);
-        //     if($resultNo->num_rows > 0) {
-            //         $rowNo = mysqli_fetch_assoc($resultNo);
-            //         $no = $rowNo['no'];
-            //         $no = intval(intval($no) + 1);
-            //     } else {
-                //         $no = "1";
-                //     }
-                //     mysqli_stmt_close();
-                // }
-                
-                $sqlNo = "SELECT `no` FROM contents ORDER BY id DESC LIMIT 1";
-                $resultNo = $conn->query($sqlNo) or die($conn->error);
-                if($resultNo->num_rows > 0) {
+$id = "id";
+$sqlNo = "SELECT `no` FROM contents ORDER BY ? DESC LIMIT 1";
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt, $sqlNo)) {
+        echo "sqlNo error";
+    } else {
+            mysqli_stmt_bind_param($stmt, "s", $id);
+            mysqli_stmt_execute($stmt);
+            $resultNo = mysqli_stmt_get_result($stmt);
+            if($resultNo->num_rows > 0) {
                     $rowNo = mysqli_fetch_assoc($resultNo);
                     $no = $rowNo['no'];
                     $no = intval(intval($no) + 1);
                 } else {
-                    $no = "1";
+                        $no = "1";
+                    }
+                    // mysqli_stmt_close();
                 }
                 
+                // $sqlNo = "SELECT `no` FROM contents ORDER BY id DESC LIMIT 1";
+                // $resultNo = $conn->query($sqlNo) or die($conn->error);
+                // if($resultNo->num_rows > 0) {
+                //     $rowNo = mysqli_fetch_assoc($resultNo);
+                //     $no = $rowNo['no'];
+                //     $no = intval(intval($no) + 1);
+                // } else {
+                //     $no = "1";
+                // }
                 
-                $sql = "
-                INSERT INTO contents
-                        (no, author, username, category, sess, zin, title, content, display, memo, created)
-                    VALUES(
-                            '{$no}',
-                            '{$author}',
-                            '{$username}',
-                            '{$category}',
-                            '{$sess}',
-                            '{$zin}',
-                            '{$title}',
-                            '{$content}',
-                            '{$display}',
-                            '{$memo}',
-                            NOW()
-                            )";
+                
+                // $sql = "
+                // INSERT INTO contents
+                //         (no, author, username, category, sess, zin, title, content, display, memo, created)
+                //     VALUES(
+                //             '{$no}',
+                //             '{$author}',
+                //             '{$username}',
+                //             '{$category}',
+                //             '{$sess}',
+                //             '{$zin}',
+                //             '{$title}',
+                //             '{$content}',
+                //             '{$display}',
+                //             '{$memo}',
+                //             NOW()
+                //             )";
                             
-                            $result = mysqli_query($conn, $sql);
-                            if($result === false){
-                                echo '저장실패. 관리자에게 문의해주세요';
-                                error_log(mysqli_error($conn));
-                            }
-                            else{
-                                echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
-                            }
+                //             $result = mysqli_query($conn, $sql);
+                //             if($result === false){
+                //                 echo '저장실패. 관리자에게 문의해주세요';
+                //                 error_log(mysqli_error($conn));
+                //             }
+                //             else{
+                //                 echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
+                //             }
                     
                     
-        //             $created = mysqli_real_escape_string($conn, NOW());
-        //             $sql = "
-        //             INSERT INTO contents
-        //                     (no, author, username, category, sess, zin, title, content, display, memo, created)
-        //                 VALUES(
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?,
-        //                         ?
-        //                         )";
+                    $created = mysqli_real_escape_string($conn, NOW());
+                    $sql = "
+                    INSERT INTO contents
+                            (no, author, username, category, sess, zin, title, content, display, memo, created)
+                        VALUES(
+                                ?,
+                                ?,
+                                ?,
+                                ?,
+                                ?,
+                                ?,
+                                ?,
+                                ?,
+                                ?,
+                                ?,
+                                ?
+                                )";
 
-        // $stmt = mysqli_stmt_init($conn);
-        // if (!mysqli_stmt_prepare($stmt, $sql)) {
-        //         echo "sql error";
-        // } else {
-        //         mysqli_stmt_bind_param($stmt, "issssssssss", $no, $author, $username, $category, $sess, $zin, $title, $content, $display, $memo, $created);
-        //         mysqli_stmt_execute($stmt);
-        //         $result = mysqli_stmt_get_result($stmt);
-        //         if($result === false){
-        //             echo '저장실패. 관리자에게 문의해주세요';
-        //             error_log(mysqli_error($conn));
-        //         }
-        //         else{
-        //             echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
-        //         }
-        //         // mysqli_stmt_close();
-        //     }
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+                echo "sql error";
+        } else {
+                mysqli_stmt_bind_param($stmt, "issssssssss", $no, $author, $username, $category, $sess, $zin, $title, $content, $display, $memo, $created);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                if($result === false){
+                    echo '저장실패. 관리자에게 문의해주세요';
+                    error_log(mysqli_error($conn));
+                }
+                else{
+                    echo("<script>alert('게시물이 생성되었습니다.');location.href='admin_contList.php';</script>");
+                }
+                // mysqli_stmt_close();
+            }
             
             
 
