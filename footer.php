7659ab264ec3fa5ba5a38cfe7c1570b645ac005d
fatihohn <fatihohn@gmail.com>
2020-06-01 23:38:44 +0900

    footer

--- footer.php
+++ footer.php
@@ -5,24 +5,27 @@
                 COPYRIGHT 2020 변방의북소리.
             </p>
             <p>
-            <div class="logo_wrap">
-                <div class="org_logo">
-                    <img src="/static/img/mcst_logo.png" alt="문화체육관광부">
+            <!-- <div class="logo_area"> -->
+            
+                <div class="logo_wrap">
+                    <div class="org_logo">
+                        <img src="/static/img/mcst_logo.png" alt="문화체육관광부">
+                    </div>
+                    <div class="org_logo">
+                        <img src="/static/img/rcda_logo.png" alt="지역문화진흥원">
+                    </div>
+                    <div class="org_logo">
+                    <a href="https://www.facebook.com/cornerstool">
+                        <img src="/static/img/cornerstool.png" alt="코너스툴">
+                    </a>
+                    </div>
+                    <div class="org_logo">
+                    <a href="https://www.facebook.com/ddcbooksori/">
+                        <img src="/static/img/farawaybooksound.png" alt="변방의 북소리">
+                    </a>
+                    </div>
                 </div>
-                <div class="org_logo">
-                    <img src="/static/img/rcda_logo.png" alt="지역문화진흥원">
-                </div>
-                <div class="org_logo">
-                <a href="https://www.facebook.com/cornerstool">
-                    <img src="/static/img/cornerstool.png" alt="코너스툴">
-                </a>
-                </div>
-                <div class="org_logo">
-                <a href="https://www.facebook.com/ddcbooksori/">
-                    <img src="/static/img/farawaybooksound.png" alt="변방의 북소리">
-                </a>
-                </div>
-            </div>
+            <!-- </div> -->
             </p>
         </div>
     </div>
