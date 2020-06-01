218f00f365352106624dc4334fa7ae640b1074c7
fatihohn <fatihohn@gmail.com>
2020-06-01 19:54:48 +0900

    description

--- bbdd_head.php
+++ bbdd_head.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="format-detection" content="telephone=no">
-    <meta name="description" content="변방의북소리 웹진 '둥둥' Doong-Doong">
+    <meta name="description" content="동두천 아카이빙 프로젝트 변방의북소리 웹진 '둥둥' Doong-Doong">
     <meta name="keywords" content="변방의북소리 웹진, 변방의 북소리, 동두천, doong-doong, doong doong, DoongDoong, doongdoong, 동두천 웹진, 동두천웹진, 매거진, webzine, 웹진 둥둥, 변방둥둥, 변방의북소리, 마을매거진, 둥둥, 동두천매거진, 동두천마을매거진, 웹진, 아카이빙, 동두천 아카이빙">
     <meta property="og:type" content="website">
     <meta property="og:title" content="변방의북소리 웹진 '둥둥' Doong-Doong">
