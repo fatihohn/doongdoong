c7d1f498d86a5b6a33cc5f4067eec0893bfe1597
fatihohn <fatihohn@gmail.com>
2020-06-02 15:18:48 +0900

    create zin prepare

--- admin_create_zin_action.php
+++ admin_create_zin_action.php
@@ -32,79 +32,137 @@ $titleCheck = $titleCheck->fetch_array();
     if($titleCheck >= 1){
 		echo "<script>alert('매거진 제목이 중복됩니다.'); history.back();</script>";
 	}else{
-
         if($publish == "ready") {
         
+                // $sql = "
+                //     INSERT INTO `zin`
+                //         (`author`, `username`, `title`, `zin_detail`, `display`, `publish`, `created`)
+                //     VALUES(
+                //         '{$author}',
+                //         '{$username}',
+                //         '{$title}',
+                //         '{$zin_detail}',
+                //         '{$display}',
+                //         '{$publish}',
+                //         NOW()
+                //         )";
                 $sql = "
-                    INSERT INTO zin
-                        (author, username, title, zin_detail, display, publish, created)
+                    INSERT INTO `zin`
+                        (`author`, `username`, `title`, `zin_detail`, `display`, `publish`, `created`)
                     VALUES(
-                        '{$author}',
-                        '{$username}',
-                        '{$title}',
-                        '{$zin_detail}',
-                        '{$display}',
-                        '{$publish}',
+                        ?,
+                        ?,
+                        ?,
+                        ?,
+                        ?,
+                        ?,
                         NOW()
                         )";
             } else if ($publish == "now") {
+                // $sql = "
+                // INSERT INTO `zin`
+                //     (`author`, `username`, `title`, `zin_detail`, `display`, `publish`, `created`)
+                // VALUES(
+                //     '{$author}',
+                //     '{$username}',
+                //     '{$title}',
+                //     '{$zin_detail}',
+                //     '{$display}',
+                //     '{$publish}',
+                //     NOW()
+                //     )
+                //     ";
                 $sql = "
-                INSERT INTO zin
-                    (author, username, title, zin_detail, display, publish, created)
+                INSERT INTO `zin`
+                    (`author`, `username`, `title`, `zin_detail`, `display`, `publish`, `created`)
                 VALUES(
-                    '{$author}',
-                    '{$username}',
-                    '{$title}',
-                    '{$zin_detail}',
-                    '{$display}',
-                    '{$publish}',
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
                     NOW()
                     )
                     ";
-                    // ON DUPLICATE KEY UPDATE `publish`='ready'
-                    // UPDATE INTO zin WHERE title !='$title'
-                    //     (publish)
-                    // VALUES('ready')  
+                 
+                // $updateSql= 
+                //     "UPDATE zin SET
+                //     `publish`='ready'
+                //     WHERE title != '$title'"; 
                 $updateSql= 
-                    "UPDATE zin SET
+                    "UPDATE `zin` SET
                     `publish`='ready'
-                    WHERE title != '$title'"; 
-                
-
+                    WHERE title != ?"; 
             }
-        
-
-
     }
-
-$result = mysqli_query($conn, $sql);
-
-if(isset($updateSql)) {
-    $resultNow = mysqli_query($conn, $updateSql);
-
-    if($result === false){
-        // if($result === false || $resultUpdate === false){
-            echo '저장실패. 관리자에게 문의해주세요';
-            error_log(mysqli_error($conn));
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+            echo "sql error";
+    } else {
+            mysqli_stmt_bind_param($stmt, "ssssss", $author, $username, $title, $zin_detail, $display, $publish);
+            
+            if(!mysqli_stmt_execute($stmt)){
+                echo '저장실패. 관리자에게 문의해주세요';
+                error_log(mysqli_error($conn));
+            }
+            else{
+                // echo("<script>alert('연재물이 생성되었습니다.');location.href='admin_thumbsList.php';</script>");
+                if(isset($updateSql)) {
+                    $stmt = mysqli_stmt_init($conn);
+                        if (!mysqli_stmt_prepare($stmt, $updateSql)) {
+                                echo "updateSql error";
+                        } else {
+                                mysqli_stmt_bind_param($stmt, "s", $title);
+                                
+                                if(!mysqli_stmt_execute($stmt)){
+                                    echo '저장실패. 관리자에게 문의해주세요';
+                                    error_log(mysqli_error($conn));
+                                }
+                                else{
+                                    echo("<script>alert('현재 발행중 매거진이 생성되었습니다.');location.href='admin_zinList.php';</script>");
+                                }
+                            }
+                } else {
+                    echo("<script>alert('매거진이 생성되었습니다.');location.href='admin_zinList.php';</script>");
+                }
+            
+            
+            
+            
+            
+            }
         }
-        else{
-            echo("<script>alert('현재 발행중 매거진이 생성되었습니다.');location.href='admin_zinList.php';</script>");
-        }
 
 
-} else {
+// // $result = mysqli_query($conn, $sql);
 
-// $resultUpdate = mysqli_query($conn, $updateSql);
-if($result === false){
-// if($result === false || $resultUpdate === false){
-    echo '저장실패. 관리자에게 문의해주세요';
-    error_log(mysqli_error($conn));
-}
-else{
-    echo("<script>alert('매거진이 생성되었습니다.');location.href='admin_zinList.php';</script>");
-}
-}
-// echo $sql;
+// if(isset($updateSql)) {
+//     $resultNow = mysqli_query($conn, $updateSql);
+
+//     if($result === false){
+//         // if($result === false || $resultUpdate === false){
+//             echo '저장실패. 관리자에게 문의해주세요';
+//             error_log(mysqli_error($conn));
+//         }
+//         else{
+//             echo("<script>alert('현재 발행중 매거진이 생성되었습니다.');location.href='admin_zinList.php';</script>");
+//         }
+
+
+// } else {
+
+// // $resultUpdate = mysqli_query($conn, $updateSql);
+// if($result === false){
+// // if($result === false || $resultUpdate === false){
+//     echo '저장실패. 관리자에게 문의해주세요';
+//     error_log(mysqli_error($conn));
+// }
+// else{
+//     echo("<script>alert('매거진이 생성되었습니다.');location.href='admin_zinList.php';</script>");
+// }
+// }
+// // echo $sql;
 
 
 ?>
