815922e51d8bb06aec2dd219e59fe258909aefd6
fatihohn <fatihohn@gmail.com>
2020-06-07 15:34:38 +0900

    backtostresc

--- admin_checkZinTitle.php
+++ admin_checkZinTitle.php
@@ -18,11 +18,11 @@
 	$member = mysqli_fetch_assoc($result);
 	
 	$dQuote = '"';
-	$sQuote = `'`;
+	$sQuote = "'";
 
 	if($member==0){
 
-		if(strpos($zinTitle, $dQuote) == true || strpos($title, $sQuote) == true) {
+		if(strpos($zinTitle, $sQuote) == true || strpos($title, $dQuote) == true) {
 			?>
 	<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 사용불가능한 매거진입니다.<div>
 <?php
