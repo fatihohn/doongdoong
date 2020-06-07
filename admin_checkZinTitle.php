5739c2acb4849c614ec3ac4da84903b385858554
fatihohn <fatihohn@gmail.com>
2020-06-07 15:00:57 +0900

    backtostresc

--- admin_checkZinTitle.php
+++ admin_checkZinTitle.php
@@ -2,14 +2,36 @@
 	include "bbdd_db_conn.php";
 	$zinTitle = $_GET["q"];
 	// $zinTitle = $_GET["username"];
-	$sql = "SELECT * FROM zin WHERE title='$zinTitle'";
-    $result = $conn->query($sql);
-    $member = mysqli_fetch_assoc($result);
-	if($member==0)
-	{
-?>
+	// $sql = "SELECT * FROM zin WHERE title='$zinTitle'";
+	$sql = "SELECT * FROM zin WHERE title=?";
+
+	$stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        // echo "sql error";
+    } else {
+    mysqli_stmt_bind_param($stmt, "s", $zinTitle);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    }
+
+    // $result = $conn->query($sql);
+	$member = mysqli_fetch_assoc($result);
+	
+	$dQuote = '"';
+
+	if($member==0){
+
+		if(strpos($zinTitle, $dQuote) == true) {
+			?>
+	<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 사용불가능한 매거진입니다.<div>
+<?php
+		} else {
+
+			
+			?>
 	<div style='font-family:"malgun gothic"';><?php echo $zinTitle; ?>는 사용가능한 매거진입니다.</div>
 <?php 
+		}
 	}else{
 ?>
 	<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 중복된 매거진입니다.<div>
