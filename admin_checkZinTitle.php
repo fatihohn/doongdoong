7b21662c5c84801586ccdd364398b41cf05d7225
fatihohn <fatihohn@gmail.com>
2020-06-09 11:32:54 +0900

    zin len

--- admin_checkZinTitle.php
+++ admin_checkZinTitle.php
@@ -20,24 +20,52 @@
 	$dQuote = '"';
 	$sQuote = '\'';
 
-	if($member==0){
+	if($member!==0){
+		?>
+			<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 중복된 매거진입니다.<div>
+		<?php
 
+
+		
+	}else{
 		// if(strpos($zinTitle, $dQuote) == true || strpos($title, $sQuote) == true) {
 		if(strpos($zinTitle, $dQuote) == true || preg_match("/'/u", $zinTitle)) {
 		
 			?>
-	<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 사용불가능한 매거진입니다.<div>
-<?php
-		} else {
+			<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 사용불가능한 매거진입니다.<div>
+			<?php
 
-			
-			?>
-	<div style='font-family:"malgun gothic"';><?php echo $zinTitle; ?>는 사용가능한 매거진입니다.</div>
-<?php 
+		} else {
+			//한글인 경우
+			if(preg_match('/[\x{3130}-\x{318F}\x{AC00}-\x{D7AF}]/u', $zinTitle)) {
+				if(mb_strlen($zinTitle, "UTF-8") >= 8) {
+					?>
+					<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 사용불가능한 매거진입니다.<div>
+					<?php
+
+				} else {
+					?>
+					<div style='font-family:"malgun gothic"';><?php echo $zinTitle; ?>는 사용가능한 매거진입니다.</div>
+					<?php 
+				}
+				//한글이 아닌 경우
+			} else {
+				if(strlen($zinTitle) >= 18) {
+					?>
+					<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 사용불가능한 매거진입니다.<div>
+					<?php
+
+				} else {
+					?>
+					<div style='font-family:"malgun gothic"';><?php echo $zinTitle; ?>는 사용가능한 매거진입니다.</div>
+					<?php 
+
+				}
+			}
+
+
+
 		}
-	}else{
-?>
-	<div style='font-family:"malgun gothic"; color:red;'><?php echo $zinTitle; ?>는 중복된 매거진입니다.<div>
-<?php
+
 	}
 ?>
