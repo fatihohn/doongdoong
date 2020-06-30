076cd9ec5faf1cf461fa8b077930651c351d9946
fatihohn <fatihohn@gmail.com>
2020-06-30 12:19:13 +0900

    footer lib logo

--- footer.php
+++ footer.php
@@ -9,20 +9,25 @@
             
                 <div class="logo_wrap">
                     <div class="org_logo">
+                        <a href="https://www.facebook.com/ddcbooksori/">
+                            <img src="/static/img/farawaybooksound.png" alt="변방의 북소리">
+                        </a>
+                    </div>
+                    <div class="org_logo">
+                        <a href="https://www.facebook.com/cornerstool">
+                            <img src="/static/img/cornerstool.png" alt="코너스툴">
+                        </a>
+                    </div>
+                    <div class="org_logo">
+                        <a href="https://cafe.naver.com/sdjihyelibrary">
+                            <img src="/static/img/small_library_horizontal.png" alt="사동초 지혜의 집">
+                        </a>
+                    </div>
+                    <div class="org_logo">
                         <img src="/static/img/mcst_logo.png" alt="문화체육관광부">
                     </div>
                     <div class="org_logo">
                         <img src="/static/img/rcda_logo.png" alt="지역문화진흥원">
-                    </div>
-                    <div class="org_logo">
-                    <a href="https://www.facebook.com/cornerstool">
-                        <img src="/static/img/cornerstool.png" alt="코너스툴">
-                    </a>
-                    </div>
-                    <div class="org_logo">
-                    <a href="https://www.facebook.com/ddcbooksori/">
-                        <img src="/static/img/farawaybooksound.png" alt="변방의 북소리">
-                    </a>
                     </div>
                 </div>
             <!-- </div> -->
