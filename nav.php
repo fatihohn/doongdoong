df5cc9fcd9a5779627a3ffbd6b6e4468f32d504f
fatihohn <fatihohn@gmail.com>
2020-07-05 10:40:02 +0900

    session vardump

--- nav.php
+++ nav.php
@@ -183,11 +183,11 @@ if ($resultCatNow->num_rows > 0) {
                         if($_SESSION['cast'] !== "normal") {
                             $indexURL = "admin_index.php";
                             echo "<a href='".$indexURL."'>";
-                            echo vardump($_SESSION['cast']);
+                            echo var_dump($_SESSION['cast']);
                         } else {
                             $indexURL = "admin_logout.php";
                             echo "<a href='".$indexURL."'>";
-                            echo vardump($_SESSION['cast']);
+                            echo var_dump($_SESSION['cast']);
                         }
                         ?>
                         <!-- <a href="<?php //echo $indexURL; ?>"> -->
