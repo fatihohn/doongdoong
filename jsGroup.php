93fadb991ea6c46b6598ac716d18ff50746657db
fatihohn <fatihohn@gmail.com>
2020-07-12 20:41:33 +0900

    cont img click

--- jsGroup.php
+++ jsGroup.php
@@ -5,3 +5,14 @@
     <script src="static/js/frontShow.js"></script>
     <script src="static/js/contView.js"></script>
     <script src="static/js/share.js"></script>
+    <script>
+        let contImgAll = document.querySelectorAll(".view_cont_content img");
+        function showImgWindow(str) {
+            imgSrc = "https://doongdoong.org/" + str;
+            window.open(imgSrc, "imgWindow", "width=1200, height=800");
+        }
+        let cia;
+        for(cia=0; cia < contImgAll.length; cia++) {
+            contImgAll[cia].addEventListener("click", function() {showImgWindow(this.title)});
+        }
+    </script>
