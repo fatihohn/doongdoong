b4fff7793036e76e014d5a9d25c9a46857fd4c3b
fatihohn <fatihohn@gmail.com>
2020-07-12 21:55:18 +0900

    frontlist column

--- frontList.php
+++ frontList.php
@@ -324,6 +324,26 @@ echo "  </ul>
     document.getElementById("standing_wrap").style.display = "none";
 }
 
+
+let contImgAll = document.querySelectorAll(".view_cont_content img");
+        function showImgWindow(str) {
+            imgSrc = "https://doongdoong.org/se2/upload/" + str;
+            window.open(imgSrc, "imgWindow", "width=1200, height=800");
+        }
+        let cia;
+        for(cia=0; cia < contImgAll.length; cia++) {
+            contImgAll[cia].addEventListener("click", function() {showImgWindow(this.title)});
+            contImgAll[cia].style.cursor = "pointer";
+        }
+
+        function frontListForm(NumberOfColumn) {
+            let standingCatAll = document.querySelectorAll(".standing_cat");
+            let stc;
+            for(stc=0; stc < standingCatAll.length; stc++) {
+                standingCatAll[stc].style.width = "calc((100% - 40px) / " + NumberOfColumn + ")";
+            }
+        }
+        
 frontListForm(<?php echo $zin_Column; ?>);
 
 </script>
