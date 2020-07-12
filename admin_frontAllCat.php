cdac81ec8f1ead1e63974c760a071f3290c314b7
fatihohn <fatihohn@gmail.com>
2020-07-12 20:15:09 +0900

    admin cat detail

--- admin_frontAllCat.php
+++ admin_frontAllCat.php
@@ -134,18 +134,20 @@ $zinTitle = $rowZinNow['title'];
             ?>
         </ul>
                     <div class = 'view_detail'>
-                        <div class = 'view_auth_detail'>
-                            <?php if($rowsAuth['auth_detail']) {
+                            <?php 
+                            echo    "<div class = 'view_auth_detail'>";
+                                if($rowsAuth['auth_detail']) {
                                 echo "글쓴이 | ".$rowsAuth['auth_detail'];
                             }
+                        echo "</div>";
                             ?>
-                        </div>
-                        <div class = 'view_cat_detail'>
-                            <?php if($rows['cat_detail']) {
+                            <?php 
+                            echo "<div class = 'view_cat_detail'>";
+                            if($rows['cat_detail']) {
                                 echo "코너 소개 | ".$rows['cat_detail'];
                             }
+                        echo "</div>";
                             ?>
-                        </div>
                     </div>
     </div>
 </div>
