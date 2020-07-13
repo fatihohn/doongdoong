38ebbd5d7fb5bdeebd68c0cf162b5d0304dfc23a
fatihohn <fatihohn@gmail.com>
2020-07-13 23:39:49 +0900

    index color func

--- frontList.php
+++ frontList.php
@@ -22,7 +22,7 @@ $zinDetail = $rowZinNow['zin_detail'];
 $sqlStandingZin = "SELECT * FROM zin WHERE publish = 'standing'";
 $resultStandingZin = $conn->query($sqlStandingZin) or die($conn->error);
 $rowStandingZin = $resultStandingZin->fetch_assoc();
-$zin_Column = $rowStandingZin['zin_column'];
+$zin_column = $rowStandingZin['zin_column'];
 $zin_color = $rowStandingZin['zin_color'];
 $title_color = $rowStandingZin['title_color'];
 $point_color = $rowStandingZin['point_color'];
@@ -343,59 +343,59 @@ function frontListForm(NumberOfColumn) {
                 }
             }
         }
-    frontListForm(<?php echo $zin_Column; ?>);
+    frontListForm(<?php echo $zin_column; ?>);
 window.addEventListener("resize", function() {
-    frontListForm(<?php echo $zin_Column; ?>);
+    frontListForm(<?php echo $zin_column; ?>);
 });
 
-function frontListColor(bgColor, titleColor, pointColor, navColor) {
-    var bodyBgColor = document.body.style.backgroundColor;
-    var hdAreaBgColor = document.getElementById("bbdd_hd_area").style.backgroundColor;
-    var scAreaBgColor = document.getElementById("bbdd_sc_area").style.backgroundColor;
-    var ftAreaBgColor = document.getElementById("bbdd_ft_area").style.backgroundColor;
-    var navBgColor = document.getElementById("bbdd_nav").style.backgroundColor;
+// function frontListColor(bgColor, titleColor, pointColor, navColor) {
+//     var bodyBgColor = document.body.style.backgroundColor;
+//     var hdAreaBgColor = document.getElementById("bbdd_hd_area").style.backgroundColor;
+//     var scAreaBgColor = document.getElementById("bbdd_sc_area").style.backgroundColor;
+//     var ftAreaBgColor = document.getElementById("bbdd_ft_area").style.backgroundColor;
+//     var navBgColor = document.getElementById("bbdd_nav").style.backgroundColor;
 
-    bodyBgColor = bgColor;
-    scAreaBgColor = bgColor;
-    ftAreaBgColor = bgColor;
+//     bodyBgColor = bgColor;
+//     scAreaBgColor = bgColor;
+//     ftAreaBgColor = bgColor;
 
-    hdAreaBgColor = pointColor;
+//     hdAreaBgColor = pointColor;
 
-    navBgColor = navColor;
+//     navBgColor = navColor;
 
-    var megaTitleAll = document.querySelectorAll(".mega_title");
-    var mta;
-    for(mta=0; mta < megaTitleAll.length; mta++) {
-        megaTitleAll[mta].style.color = titleColor;
-        megaTitleAll[mta].style.borderBottom = "2px dashed" + titleColor;
-        document.a.style.color = titleColor;
-    }
+//     var megaTitleAll = document.querySelectorAll(".mega_title");
+//     var mta;
+//     for(mta=0; mta < megaTitleAll.length; mta++) {
+//         megaTitleAll[mta].style.color = titleColor;
+//         megaTitleAll[mta].style.borderBottom = "2px dashed" + titleColor;
+//         document.a.style.color = titleColor;
+//     }
 
-    var navCloseBtn = document.querySelectorAll(".close");
-    var ncb;
-    for(ncb=0; ncb < navCloseBtn.length; ncb++) {
-        navCloseBtn[ncb].style.color = pointColor;
-    }
+//     var navCloseBtn = document.querySelectorAll(".close");
+//     var ncb;
+//     for(ncb=0; ncb < navCloseBtn.length; ncb++) {
+//         navCloseBtn[ncb].style.color = pointColor;
+//     }
 
-    var navPortalBtn = document.querySelectorAll(".portal_btn");
-    var npb;
-    for(npb=0; npb < navPortalBtn.length; npb++) {
-        navPortalBtn[npb].style.color = pointColor;
-    }
+//     var navPortalBtn = document.querySelectorAll(".portal_btn");
+//     var npb;
+//     for(npb=0; npb < navPortalBtn.length; npb++) {
+//         navPortalBtn[npb].style.color = pointColor;
+//     }
 
-    var navMain = document.querySelectorAll(".nav_main");
-    var nMn;
-    for(nMn=0; nMn < navMain.length; nMn++) {
-        navMain[nMn].style.border = "2px dashed" + pointColor;
-    }
+//     var navMain = document.querySelectorAll(".nav_main");
+//     var nMn;
+//     for(nMn=0; nMn < navMain.length; nMn++) {
+//         navMain[nMn].style.border = "2px dashed" + pointColor;
+//     }
 
-    var navFontColor = document.querySelectorAll(".nav_font");
-    var nfc;
-    for(nfc=0; nfc < navFontColor.length; nfc++) {
-        navFontColor[nfc].color = pointColor;
-    }
-}
+//     var navFontColor = document.querySelectorAll(".nav_font");
+//     var nfc;
+//     for(nfc=0; nfc < navFontColor.length; nfc++) {
+//         navFontColor[nfc].color = pointColor;
+//     }
+// }
 
-frontListColor("<?php echo $zin_color; ?>", "<?php echo $title_color; ?>", "<?php echo $point_color; ?>", "<?php echo $nav_color; ?>");
+// frontListColor("<?php //echo $zin_color; ?>", "<?php //echo $title_color; ?>", "<?php //echo $point_color; ?>", "<?php //echo $nav_color; ?>");
 
 </script>
--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php include "bbdd_head.php"; ?>
+  <?php include "bbdd_head.php"; 
+  
+  $sqlStandingZin = "SELECT * FROM zin WHERE publish = 'standing'";
+$resultStandingZin = $conn->query($sqlStandingZin) or die($conn->error);
+$rowStandingZin = $resultStandingZin->fetch_assoc();
+$zin_column = $rowStandingZin['zin_column'];
+$zin_color = $rowStandingZin['zin_color'];
+$title_color = $rowStandingZin['title_color'];
+$point_color = $rowStandingZin['point_color'];
+$nav_color = $rowStandingZin['nav_color'];
+
+  ?>
   
 </head>
 <body oncontextmenu="return false" ondragstart="return false" onselectstart="return false">
@@ -42,7 +53,59 @@
     
     <?php include "jsGroup.php"; ?>
 
+<script>
+    
+function frontListColor(bgColor, titleColor, pointColor, navColor) {
+    var bodyBgColor = document.body.style.backgroundColor;
+    var hdAreaBgColor = document.getElementById("bbdd_hd_area").style.backgroundColor;
+    var scAreaBgColor = document.getElementById("bbdd_sc_area").style.backgroundColor;
+    var ftAreaBgColor = document.getElementById("bbdd_ft_area").style.backgroundColor;
+    var navBgColor = document.getElementById("bbdd_nav").style.backgroundColor;
 
+    bodyBgColor = bgColor;
+    scAreaBgColor = bgColor;
+    ftAreaBgColor = bgColor;
+
+    hdAreaBgColor = pointColor;
+
+    navBgColor = navColor;
+
+    var megaTitleAll = document.querySelectorAll(".mega_title");
+    var mta;
+    for(mta=0; mta < megaTitleAll.length; mta++) {
+        megaTitleAll[mta].style.color = titleColor;
+        megaTitleAll[mta].style.borderBottom = "2px dashed" + titleColor;
+        document.a.style.color = titleColor;
+    }
+
+    var navCloseBtn = document.querySelectorAll(".close");
+    var ncb;
+    for(ncb=0; ncb < navCloseBtn.length; ncb++) {
+        navCloseBtn[ncb].style.color = pointColor;
+    }
+
+    var navPortalBtn = document.querySelectorAll(".portal_btn");
+    var npb;
+    for(npb=0; npb < navPortalBtn.length; npb++) {
+        navPortalBtn[npb].style.color = pointColor;
+    }
+
+    var navMain = document.querySelectorAll(".nav_main");
+    var nMn;
+    for(nMn=0; nMn < navMain.length; nMn++) {
+        navMain[nMn].style.border = "2px dashed" + pointColor;
+    }
+
+    var navFontColor = document.querySelectorAll(".nav_font");
+    var nfc;
+    for(nfc=0; nfc < navFontColor.length; nfc++) {
+        navFontColor[nfc].color = pointColor;
+    }
+}
+
+frontListColor("<?php echo $zin_color; ?>", "<?php echo $title_color; ?>", "<?php echo $point_color; ?>", "<?php echo $nav_color; ?>");
+
+</script>
 
 </body>
 </html>
