c415c82194d1d9a9d9663582ca5b4f5e166e5a80
fatihohn <fatihohn@gmail.com>
2020-07-14 21:06:37 +0900

    show img window func

--- jsGroup.php
+++ jsGroup.php
@@ -14,7 +14,7 @@
         let cia;
         for(cia=0; cia < contImgAll.length; cia++) {
             if(contImgAll[cia].title) {
-                if(contImgAll[cia].src == "se2/upload/" + contImgAll[cia].title) {
+                if(contImgAll[cia].src == "https://doongdoong.org/se2/upload/" + contImgAll[cia].title) {
                     contImgAll[cia].addEventListener("click", function() {showImgWindow(this.title)});
                     contImgAll[cia].style.cursor = "pointer";
                 }
