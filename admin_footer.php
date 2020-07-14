88995cecd883f914c3c350a8b7a01a51e0d501b8
fatihohn <fatihohn@gmail.com>
2020-07-14 20:44:26 +0900

    footer corner coder

--- admin_footer.php
+++ admin_footer.php
@@ -2,7 +2,7 @@
  echo'
  <div id="footercontent">
  <p>Copyright 2020 © 변방의북소리. All rights reserved</p>
-            <p>변방의북소리 대표자 : 이경렬 | 개발자 : 코너코더</p>
+            <p>변방의북소리 대표자 : 이경렬 | 개발자 : 구석진개발자</p>
         </div>
   ';
 
