07e94ccd752b23eaab95cf97ccb70bb71ffd4013
fatihohn <fatihohn@gmail.com>
2020-07-18 19:20:32 +0900

    img slide init

--- jsGroup.php
+++ jsGroup.php
@@ -12,7 +12,7 @@
             function showImgWindow(str) {
                 let imgSrc = "https://doongdoong.org/se2/upload/" + str;
                 // window.open(imgSrc, "imgWindow", "width=1200, height=800");
-                let imgSlide = document.body.createElement("div");
+                let imgSlide = document.createElement("div");
                 imgSlide.className = "img_slide";
                 imgSlide.style.width = "100vw";
                 imgSlide.style.height = "100vh";
@@ -23,6 +23,9 @@
                 imgSlide.style.backgroundSize = 'contain';
                 imgSlide.style.backgroundRepeat = 'no-repeat';
                 imgSlide.style.zIndex = '9999';
+
+                document.appendChild(imgSlide);
+
                 imgSlide.onclick = function() {
                     imgSlide.remove();
                 };
