bba299992f777ddfebdec67ef87c5b022f2f1b2e
fatihohn <fatihohn@gmail.com>
2020-07-19 14:28:30 +0900

    img slide

--- static/js/contView.js
+++ static/js/contView.js
@@ -18,7 +18,7 @@ for(a = 0; a < contImgs.length; a++) {
     contImgs[a].parentElement.style.textIndent = "0";
     contImgs[a].style.maxWidth = "100%";
     contImgs[a].style.height = "auto";
-    contImgs[a].id = a;
+    contImgs[a].classname = a;
 
     if(contImgs.length < 2) {
 
