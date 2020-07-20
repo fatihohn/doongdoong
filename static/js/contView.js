5b41d5be9d3fe5e22b56fb92f18f1de5224797d5
fatihohn <fatihohn@gmail.com>
2020-07-20 18:54:38 +0900

    img slide

--- static/js/contView.js
+++ static/js/contView.js
@@ -11,10 +11,10 @@
         // }
         // imgIndent();
 
-        
+
         let contImgs = document.querySelectorAll(".view_cont_content img");
 
-        
+
 
         let cia;
         for (cia = 0; cia < contImgs.length; cia++) {
@@ -25,7 +25,7 @@
             function showNextImg(imgClassNext) {
                 let imgClassNextInt = parseInt(imgClassNext);
                 let nextImgSrc;
-                if(imgClassNext == contImgs.length) {
+                if (imgClassNext == contImgs.length) {
                     nextImgSrc = null;
                 } else {
                     // nextImgSrc = contImgs[imgClassNext + 1].src;
@@ -44,9 +44,10 @@
                     document.getElementById("img_slide_prev").className = imgClassNextInt + 1;
                 }
             }
+
             function showPrevImg(imgClassPrev) {
                 let prevImgSrc;
-                if(imgClassPrev == 0) {
+                if (imgClassPrev == 0) {
                     prevImgSrc = null;
                 } else {
                     prevImgSrc = contImgs[imgClassPrev - 1].src;
@@ -66,6 +67,7 @@
             function viewImgClick() {
                 // let contImgAll = document.querySelectorAll(".view_cont_content img");
                 let introTitle = document.getElementById("intro_title");
+
                 function showImgWindow(imgUrl, imgClassName) {
                     // let imgSrc = "https://doongdoong.org/se2/upload/" + imgurl;
                     let imgSrc = imgUrl;
@@ -75,11 +77,11 @@
                     imgSlide.style.width = "100%";
                     imgSlide.style.height = "100%";
                     imgSlide.style.maxHeight = "100vh";
-                    imgSlide.style.backgroundImage = 
-                    'url(' +
-                    imgSrc +
-                    ')';
-                    if(window.innerWidth > 801) {
+                    imgSlide.style.backgroundImage =
+                        'url(' +
+                        imgSrc +
+                        ')';
+                    if (window.innerWidth > 801) {
                         imgSlide.style.backgroundPosition = 'center center';
                     } else {
                         imgSlide.style.backgroundPosition = 'center center';
@@ -97,14 +99,14 @@
                     imgSlide.style.right = '0';
                     imgSlide.style.transition = '0.5s';
                     imgSlide.style.animation = 'expand 0.5s ease-in-out';
-                    
+
                     setTimeout(
                         document.body.appendChild(imgSlide), 500
                     );
-                    
-    
+
+
                     // if(contImgs.length < 2) {
-    
+
                     // } else if (contImgs.length >= 2 && imgClassName == 0) {
                     //     var nextImgSrc = contImgs[nextImgNumber].src;
                     //     var prevImgSrc = 'noCont';
@@ -115,17 +117,17 @@
                     //     var nextImgSrc = 'noCont';
                     //     var prevImgSrc = contImgs[prevImgNumber].src;
                     // }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
                     var imgSlideBtnNext = document.createElement("div");
                     // imgSlideBtnNext.className = "img_slide_btn";
                     imgSlideBtnNext.className = imgClassName;
                     imgSlideBtnNext.id = "img_slide_next";
-                    
+
                     imgSlideBtnNext.innerHTML = "▶";
                     imgSlideBtnNext.style.width = "60px";
                     imgSlideBtnNext.style.height = "60px";
@@ -134,40 +136,40 @@
                     imgSlideBtnNext.style.color = "white";
                     imgSlideBtnNext.style.opacity = "0.3";
                     imgSlideBtnNext.style.position = "fixed";
-                    imgSlideBtnNext.style.top = "calc(50% - 60px)";
+                    imgSlideBtnNext.style.top = "calc(50% - 30px)";
                     imgSlideBtnNext.style.right = "0";
                     imgSlideBtnNext.style.margin = "10px";
                     imgSlideBtnNext.style.zIndex = "9999";
                     imgSlideBtnNext.style.cursor = "pointer";
                     imgSlideBtnNext.style.textAlign = "center";
-    
-    
+
+
                     // let nextImgNumber = imgSlideBtnNext.className + 1;
                     // if(nextImgNumber <= contImgs.length) {
                     //     let nextImgSrc = contImgs[nextImgNumber].src;
                     // }
-                    
-                    
-                    
-                    
-    
-    
+
+
+
+
+
+
                     imgSlideBtnNext.onmouseover = function() {
                         imgSlideBtnNext.style.opacity = "0.9";
                     }
                     imgSlideBtnNext.onmouseleave = function() {
-                        imgSlideBtnNext.style.opacity = "0.3";
-                    }
-                    // imgSlideBtnNext.onclick = function() {
-                    //     showNextImg(this.className);
-                    // }
-                    
-                    
+                            imgSlideBtnNext.style.opacity = "0.3";
+                        }
+                        // imgSlideBtnNext.onclick = function() {
+                        //     showNextImg(this.className);
+                        // }
+
+
                     var imgSlideBtnPrev = document.createElement("div");
                     // imgSlideBtnPrev.className = "img_slide_btn";
                     imgSlideBtnPrev.className = imgClassName;
                     imgSlideBtnPrev.id = "img_slide_prev";
-                    
+
                     imgSlideBtnPrev.innerHTML = "◀";
                     imgSlideBtnPrev.style.width = "60px";
                     imgSlideBtnPrev.style.height = "60px";
@@ -176,142 +178,142 @@
                     imgSlideBtnPrev.style.color = "white";
                     imgSlideBtnPrev.style.opacity = "0.3";
                     imgSlideBtnPrev.style.position = "fixed";
-                    imgSlideBtnPrev.style.top = "calc(50% - 60px)";
+                    imgSlideBtnPrev.style.top = "calc(50% - 30px)";
                     imgSlideBtnPrev.style.left = "0";
                     imgSlideBtnPrev.style.margin = "10px";
                     imgSlideBtnPrev.style.zIndex = "9999";
                     imgSlideBtnPrev.style.cursor = "pointer";
                     imgSlideBtnPrev.style.textAlign = "center";
-                    
-                    
+
+
                     // let prevImgNumber = imgSlideBtnPrev.className - 1;
                     // if(prevImgNumber >= 0) {
-                        //     let prevImgSrc = contImgs[prevImgNumber].src;
-                        // }
-                        
-                        
-                        
-                        imgSlideBtnPrev.onmouseover = function() {
-                            imgSlideBtnPrev.style.opacity = "0.9";
-                        }
-                        imgSlideBtnPrev.onmouseleave = function() {
+                    //     let prevImgSrc = contImgs[prevImgNumber].src;
+                    // }
+
+
+
+                    imgSlideBtnPrev.onmouseover = function() {
+                        imgSlideBtnPrev.style.opacity = "0.9";
+                    }
+                    imgSlideBtnPrev.onmouseleave = function() {
                             imgSlideBtnPrev.style.opacity = "0.3";
                         }
                         // imgSlideBtnPrev.onclick = function(imgClassName) {
-                            //     if(prevImgSrc !== null) {
-                                //         imgSlide.style.backgroundImage = 
-                                //         'url(' +
-                                //         prevImgSrc +
-                                //         ')';
-                                //     }
-                                // }
-                                
-                                // imgSlideBtnPrev.onclick = function() {
-                                    //         showPrevImg(this.className);
-                                    //     }
-                                    
+                        //     if(prevImgSrc !== null) {
+                        //         imgSlide.style.backgroundImage = 
+                        //         'url(' +
+                        //         prevImgSrc +
+                        //         ')';
+                        //     }
+                        // }
 
-                        var imgSlideBtnEsc = document.createElement("div");
-                        
-                        imgSlideBtnEsc.id = "img_slide_esc";
-                        imgSlideBtnEsc.style.width = "calc(100% - 200px)";
-                        imgSlideBtnEsc.style.height = "80%";
-                        imgSlideBtnEsc.style.margin = "auto";
-                        imgSlideBtnEsc.style.position = "relative";
-                        imgSlideBtnEsc.style.cursor = "pointer";
-                        imgSlideBtnEsc.title = "닫기";
+                    // imgSlideBtnPrev.onclick = function() {
+                    //         showPrevImg(this.className);
+                    //     }
 
 
-                                    
-                        imgSlide.appendChild(imgSlideBtnEsc);
-                        imgSlide.appendChild(imgSlideBtnNext);
-                        imgSlide.appendChild(imgSlideBtnPrev);
-                                    
+                    var imgSlideBtnEsc = document.createElement("div");
 
-                        let imgSlideNext = document.getElementById("img_slide_next");
-                        let imgSlidePrev = document.getElementById("img_slide_prev");
-                        document.getElementById("img_slide_next").onclick = function() {
-                            showNextImg(this.className);
-                        }
-                        document.getElementById("img_slide_prev").onclick = function() {
-                            showPrevImg(this.className);
-                        }
-                        document.onkeydown = function(keyDown) {
+                    imgSlideBtnEsc.id = "img_slide_esc";
+                    imgSlideBtnEsc.style.width = "calc(100% - 200px)";
+                    imgSlideBtnEsc.style.height = "80%";
+                    imgSlideBtnEsc.style.margin = "auto";
+                    imgSlideBtnEsc.style.position = "relative";
+                    imgSlideBtnEsc.style.cursor = "pointer";
+                    imgSlideBtnEsc.title = "닫기";
+
+
+
+                    imgSlide.appendChild(imgSlideBtnEsc);
+                    imgSlide.appendChild(imgSlideBtnNext);
+                    imgSlide.appendChild(imgSlideBtnPrev);
+
+
+                    let imgSlideNext = document.getElementById("img_slide_next");
+                    let imgSlidePrev = document.getElementById("img_slide_prev");
+                    document.getElementById("img_slide_next").onclick = function() {
+                        showNextImg(this.className);
+                    }
+                    document.getElementById("img_slide_prev").onclick = function() {
+                        showPrevImg(this.className);
+                    }
+                    document.onkeydown = function(keyDown) {
                             switch (keyDown.keyCode) {
                                 case 37:
                                     // str = 'Left Key pressed!'; 
-                                    if(imgSlideNext && imgSlidePrev) {
-                                    // projects_contentsList(mPrev);
-                                    showPrevImg(imgSlidePrev.className);
-                                    
-                                    } 
+                                    if (imgSlideNext && imgSlidePrev) {
+                                        // projects_contentsList(mPrev);
+                                        showPrevImg(imgSlidePrev.className);
+
+                                    }
                                     // console.log("Left Key Pressed!")
                                     break;
                                 case 39:
                                     // str = 'Right Key pressed!'; 
-                                    if(imgSlideNext && imgSlidePrev) {
-                                    // projects_contentsList(mNext);
-                                    showNextImg(imgSlideNext.className);
-                                    
-                                    } 
+                                    if (imgSlideNext && imgSlidePrev) {
+                                        // projects_contentsList(mNext);
+                                        showNextImg(imgSlideNext.className);
+
+                                    }
                                     // console.log("Right Key Pressed!")
                                     break;
                                 case 27:
                                     // str = 'Esc Key pressed!'; 
-                                    if(imgSlideNext && imgSlidePrev) {
+                                    if (imgSlideNext && imgSlidePrev) {
                                         imgSlide.remove();
-                                       
-                                    } 
-                                    
-                                    
-                                        // console.log("Esc Key Pressed!")
+
+                                    }
+
+
+                                    // console.log("Esc Key Pressed!")
                                     break;
                             }
                         }
-                    // imgSlideBtnNext.addEventListener("click", showNextImg(this.class));
-                    // imgSlideBtnPrev.addEventListener("click", showPrevImg(this.class));
-                    
+                        // imgSlideBtnNext.addEventListener("click", showNextImg(this.class));
+                        // imgSlideBtnPrev.addEventListener("click", showPrevImg(this.class));
+
                     imgSlideBtnEsc.onclick = function() {
                         imgSlide.remove();
                     };
-    
-    
+
+
                 }
                 // let cia;
                 // for(cia=0; cia < contImgAll.length; cia++) {
-    
-                    if(contImgs[cia].title) {
-                        if(introTitle) {
-    
-                        } else {
-                            // if(window.innerWidth > 801) {
-                            if(window.innerWidth > 1) {
-                                contImgs[cia].addEventListener("click", function() {
-                                    showImgWindow(this.src, this.className);     
-                                });
-                                contImgs[cia].style.cursor = "pointer";
-                                contImgs[cia].className = cia;
-                                
-                            } 
+
+                if (contImgs[cia].title) {
+                    if (introTitle) {
+
+                    } else {
+                        // if(window.innerWidth > 801) {
+                        if (window.innerWidth > 1) {
+                            contImgs[cia].addEventListener("click", function() {
+                                showImgWindow(this.src, this.className);
+                            });
+                            contImgs[cia].style.cursor = "pointer";
+                            contImgs[cia].className = cia;
+
                         }
                     }
+                }
 
-                    
-            
-                    // function showPrevImg(imgClass) {
-                    //     let prevImgSrc = contImgs[imgClass - 1].src;
-                    //     if (prevImgSrc !== null) {
-                    //         imgSlide.style.backgroundImage =
-                    //             'url(' +
-                    //             prevImgSrc +
-                    //             ')';
-                    //     }
-                    // }
-                    // }
-                    
+
+
+                // function showPrevImg(imgClass) {
+                //     let prevImgSrc = contImgs[imgClass - 1].src;
+                //     if (prevImgSrc !== null) {
+                //         imgSlide.style.backgroundImage =
+                //             'url(' +
+                //             prevImgSrc +
+                //             ')';
+                //     }
+                // }
+                // }
+
             }
             viewImgClick();
-            
+
             // if(window.innerWidth < 801) {
             //     document.getElementById("img_slide").style.backgroundPosition = "center top";
 
