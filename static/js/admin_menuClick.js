a1d82be3f3e08c81c83ec85ee221782eb8e98fb6
fatihohn <fatihohn@gmail.com>
2020-08-05 18:56:50 +0900

    admenu func

--- static/js/admin_menuClick.js
+++ static/js/admin_menuClick.js
@@ -1,26 +1,54 @@
+// let adMenu = document.querySelectorAll(".adminMenu");
+// let adMenuLi = document.querySelector(".adArticle");
+// // let adMenuLi = document.querySelector(".adminMenuLi");
+// let adMenuLiDis = adMenuLi.style.display;
+
+// let mL;
+// for(mL = 0; mL < adMenu.length; mL++) {
+// adMenu[mL].onclick = function() {
+//     if(adMenuLiDis == "none") {
+//     adMenuLi.style.display = "initial";
+//     adMenuLiDis = "initial";
+// } else if(adMenuLiDis !=="none") {
+//     adMenuLi.style.display = "none";
+//     adMenuLiDis = "none";
+// } else {
+//     adMenuLi.style.display = "initial";
+//     adMenuLiDis = "initial";
+//     }
+// }
+// }
+// function initAdMenu () {
+//     adMenuLi.style.display = "none";
+//     adMenuLiDis = "none";
+// }
+
+// initAdMenu();
+
 let adMenu = document.querySelectorAll(".adminMenu");
 let adMenuLi = document.querySelector(".adArticle");
 // let adMenuLi = document.querySelector(".adminMenuLi");
-let adMenuLiDis = adMenuLi.style.display;
+let adMenuLiDis = adMenuLi.style.visibility;
 
 let mL;
-for(mL = 0; mL < adMenu.length; mL++) {
-adMenu[mL].onclick = function() {
-    if(adMenuLiDis == "none") {
-    adMenuLi.style.display = "initial";
-    adMenuLiDis = "initial";
-} else if(adMenuLiDis !=="none") {
-    adMenuLi.style.display = "none";
-    adMenuLiDis = "none";
-} else {
-    adMenuLi.style.display = "initial";
-    adMenuLiDis = "initial";
+for (mL = 0; mL < adMenu.length; mL++) {
+    adMenu[mL].onclick = function() {
+        if (adMenuLiDis == "hidden") {
+            adMenuLi.style.visibility = "visible";
+            adMenuLiDis = "visible";
+        } else if (adMenuLiDis !== "hidden") {
+            adMenuLi.style.visibility = "hidden";
+            adMenuLiDis = "hidden";
+        } else {
+            adMenuLi.style.visibility = "visible";
+            adMenuLiDis = "visible";
+        }
     }
 }
-}
-function initAdMenu () {
-    adMenuLi.style.display = "none";
-    adMenuLiDis = "none";
+
+function initAdMenu() {
+    adMenuLi.style.visibility = "hidden";
+    adMenuLiDis = "hidden";
 }
 
 initAdMenu();
