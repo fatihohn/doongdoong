7ac0beac56713831c94de7ecc5ddfa3c8ce9f930
fatihohn <fatihohn@gmail.com>
2020-09-26 12:50:12 +0900

    admin front standing

--- admin_frontList.php
+++ admin_frontList.php
@@ -80,13 +80,15 @@ if ($resultCatPast->num_rows >= 1) {
         $latestCatPastCont = $rowCatPastCont['created'];
         
         $catTitlePast = $rowCatPast['category'];
-        $sqlContPast = "SELECT * FROM contents WHERE zin!=? AND category=? AND display='on'";
+        // $sqlContPast = "SELECT * FROM contents WHERE zin!=? AND category=? AND display='on'";
+        $sqlContPast = "SELECT * FROM contents WHERE zin=? AND category=? AND display='on'";
       
         
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sqlContPast)) {
         } else {
-                mysqli_stmt_bind_param($stmt, "ss", $zinTitle, $catTitlePast);
+                // mysqli_stmt_bind_param($stmt, "ss", $zinTitle, $catTitlePast);
+                mysqli_stmt_bind_param($stmt, "ss", $standingZinTitle, $catTitlePast);
                 mysqli_stmt_execute($stmt);
                 $resultContPast = mysqli_stmt_get_result($stmt);
         }
